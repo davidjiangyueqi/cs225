4626e4f27bf39cbeb33eb0bb3f54cff1722aac60
Yueqi Jiang <yueqij2@linux-a2.ews.illinois.edu>
2020-08-27 16:09:06 -0500

    complete part 1/2

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -3,9 +3,40 @@
  * Implementation of the HSLAPixel class for use in with the PNG library.
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #include "HSLAPixel.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
 
 namespace cs225 {
+
+    //default constructor
+    cs225::HSLAPixel::HSLAPixel() {
+        a = 1;
+        h = 0;
+        s = 0;
+        l = 1;
+    }
+
+    cs225::HSLAPixel::HSLAPixel	(double hue,
+                                 double saturation,
+                                 double luminance) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = 1;
+    }
+
+    cs225::HSLAPixel::HSLAPixel (double hue,
+                                 double saturation,
+                                 double luminance,
+                                 double alpha) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = alpha;
+    }
 }
--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -6,6 +6,16 @@
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
+  cs225::PNG InputImage, OutputImage;
+  InputImage.readFromFile(inputFile);
+  OutputImage = InputImage;
+  for (unsigned height = 0; height < InputImage.height(); height++) {
+    for (unsigned width = 0; width < InputImage.width(); width++) {
+      cs225::HSLAPixel & CurPixel = InputImage.getPixel(width, height);
+      OutputImage.getPixel(InputImage.width() - width - 1, InputImage.height() - height - 1) = CurPixel;
+    }
+  }
+  OutputImage.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
