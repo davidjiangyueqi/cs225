3d40b4d11d46f43f25dac80fdb906446db2b7de4
Evans <gcevans@cs225-adm.cs.illinois.edu>
2020-09-22 13:48:33 -0500

    remove extra stickers

--- mp_stickers/release/Image.h
+++
@@ -1,7 +0,0 @@
-/**
- * @file Image.h
- * Contains your declaration of the interface for the Image class.
- */
-
-#pragma once
-
--- mp_stickers/release/StickerSheet.h
+++
@@ -1,7 +0,0 @@
-/**
- * @file StickerSheet.h
- * Contains your declaration of the interface for the StickerSheet class.
- */
-#pragma once
-
- 
