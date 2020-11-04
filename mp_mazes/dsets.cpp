b97b0355cbfc222c376ed1c3db40d1d08a7f348a
Yueqi Jiang <yueqij2@linux-a2.ews.illinois.edu>
2020-11-04 16:20:35 -0600

    complete mazes part1

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,45 @@
-/* Your code here! */
+#include "dsets.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+void DisjointSets::addelements(int num) {
+    
+    for (int i = 0; i < num; i++) {
+        _elems.push_back(-1);
+    }
+}
+
+int  DisjointSets::find(int elem) {
+    
+    //base case root
+    if (_elems[elem] < 0) return elem;
+    //recurse up
+    int root = find(_elems[elem]);
+    _elems[elem] = root;
+    return root;
+}
+
+void DisjointSets::setunion(int a, int b) {
+
+    //find root of the 2 set
+    while (_elems[a] > -1) a = _elems[a];
+    while (_elems[b] > -1) b = _elems[b];
+
+    int new_height_ = _elems[a] + _elems[b];
+    //a shorter
+    if (_elems[a] > _elems[b]) {
+        _elems[a] = b;
+        _elems[b] = new_height_;
+    } else {
+        _elems[b] = a;
+        _elems[a] = new_height_;
+    }
+}
+
+int DisjointSets::size(int elem) {
+
+    while (_elems[elem] > -1) elem = _elems[elem];
+    return _elems[elem] * (-1);
+}
--- mp_mazes/dsets.h
+++ mp_mazes/dsets.h
@@ -1,1 +1,17 @@
-/* Your code here! */
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class DisjointSets {
+
+    public:
+        void addelements(int num);
+        int find(int elem);
+        void setunion(int a, int b);
+        int size(int elem);
+
+    private:
+        std::vector<int> _elems;
+};
